3c5e057ed0b3dec07d350014172b3f03e896f0cd
Martin Logan <martinjlogan@gmail.com>
2011-09-11 17:08:18 -0500

    added the sort function to todo_list.erl

--- todo_list.erl
+++ todo_list.erl
@@ -16,19 +16,20 @@
 %%%===================================================================
 
 %% @doc write out a todo list
--spec write_todo(list()) -> ok.
+-type todo_list() :: [{non_neg_integer(), term()}].
+-spec write_todo(todo_list()) -> ok.
 write_todo(ToDo) ->
-    file:write_file("todo.bin", term_to_binary(ToDo)).
+    file:write_file("todo.bin", term_to_binary(sort_todo(ToDo))).
 
 %% @doc read back out a todo list
--spec read_todo(list()) -> string().
-read_todo(ToDo) ->
+-spec read_todo() -> list().
+read_todo() ->
     {ok, Bin} = file:read_file("todo.bin"),
     binary_to_term(Bin).
-
-
-    
 
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+sort_todo(ToDo) ->
+    lists:sort(fun({P1, N1}, {P1, N2}) -> N1 < N2 end,
+	       ToDo).
